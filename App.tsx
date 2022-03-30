46f5d997249d9aafe680e1019b1e7135ae2f9129
develcode01 <michael.junges@develcode.com.br>
2022-03-30 16:39:05 -0300

    first

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { Dashboard } from './src/screens/Dashboard'
+import theme from './src/global/styles/theme'
+import { ThemeProvider } from "styled-components"
+
+export function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <Dashboard />
+        </ThemeProvider>
+
+    )
+}
+
